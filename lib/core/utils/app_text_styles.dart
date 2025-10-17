8d26b525d47fbdebe6332dd8864dbd16c9df6a83
Mufeed Mamadan <mufeed.kodika@gmail.com>
2025-10-18 02:34:12 +0300

    "Added import statement for app_text_styles and no other changes"

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle styleBold13 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 13,
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+
+  static const TextStyle styleBold19 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 19,
+  );
+
+  static const TextStyle styleBold23 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 23,
+  );
+
+  static const TextStyle styleBold28 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 28,
+  );
+
+  ///////////////////////////////////////////////
+
+  static const TextStyle styleSemiBold11 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+  );
+
+  static const TextStyle styleSemiBold13 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  ///////////////////////////////////////////////
+
+  static const TextStyle styleMedium15 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 15,
+  );
+
+  ///////////////////////////////////////////////
+
+  static const TextStyle styleRegular13 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+  );
+
+  static const TextStyle styleRegular16 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const TextStyle styleRegular22 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 22,
+  );
+
+  static const TextStyle styleRegular26 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 26,
+  );
+}
--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prestige_app/features/auth/presentation/views/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -6,6 +7,22 @@ class LoginView extends StatelessWidget {
   static const routeName = 'login';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w900,
+          ),
+        )
+      ),
+      body: const LoginViewBody()
+    );
   }
 }
